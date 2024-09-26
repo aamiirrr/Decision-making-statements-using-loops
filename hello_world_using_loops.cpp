873e5bf21819479d8ba3fcd2ec3e9f23a3bd3c24
Aamir Ayaan Quraishi <aamir.quraishi.btech2023@sitpune.edu.in>
2024-09-26 11:34:02 +0530

    Create hello_world_using_loops.cpp

---
+++ hello_world_using_loops.cpp
@@ -0,0 +1,24 @@
+//Aamir Ayaan Quraishi
+//23070123002
+//Aim - print hello world using for loop for n number of times.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    for (i = 1; i <= 5; i++)
+    {
+        cout << "hello world" << endl;
+    }
+    return 0;
+}
+
+/* 
+OUTPUT
+hello world
+hello world
+hello world
+hello world
+hello world
+*/
