d4110afed09c59b4626896e9b3ebc9a4ce84fc8e
Aamir Ayaan Quraishi <aamir.quraishi.btech2023@sitpune.edu.in>
2024-09-26 11:31:53 +0530

    Create do_while_loops.cpp

---
+++ do_while_loops.cpp
@@ -0,0 +1,27 @@
+//Aamir Ayaan Quraishi
+//23070123002
+//Aim - take input from user and end the loop if the user enter 'SIT' .
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    
+    do {
+        cout << "Enter the Name: ";
+        cin >> name;
+        if (name != "SIT") {
+            cout << name << endl;
+        }
+    } while (name != "SIT");
+}
+
+/*
+OUTPUT 
+Enter the Name: sit
+sit
+Enter the Name: jit
+jit
+Enter the Name: SIT
+*/
