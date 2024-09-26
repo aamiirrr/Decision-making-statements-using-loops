7b41f810799e292484545cc7aa46e005e3888cde
Aamir Ayaan Quraishi <aamir.quraishi.btech2023@sitpune.edu.in>
2024-09-26 11:36:24 +0530

    Create reversing_your_PRN.cpp

---
+++ reversing_your_PRN.cpp
@@ -0,0 +1,27 @@
+//Aamir Ayaan Quraishi
+//23070123002
+//Aim - To reverse your PRN using while loops.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+     int PRN,reverse;
+    cout<<"Enter your PRN:"<<endl;
+    cin>>PRN;
+    while(PRN!=0){
+        reverse=PRN%10;
+        PRN=PRN/10;
+        cout<<reverse;
+    }
+        
+    return 0;
+}
+
+
+/*
+Output
+Enter your PRN:
+23070
+07032
+*/
