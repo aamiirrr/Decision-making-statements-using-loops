fd46f4b77f253d16506fe343f82f40bdaf07267b
Aamir Ayaan Quraishi <aamir.quraishi.btech2023@sitpune.edu.in>
2024-09-26 11:35:54 +0530

    Create print_upto_20.cpp

---
+++ print_upto_20.cpp
@@ -0,0 +1,39 @@
+//Aamir Ayaan Quraishi
+//23070123002
+//Aim - to print upto 20 using while loops.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=1;
+    while(i<=20){
+        cout<<i << endl;
+        i++;
+            
+    }
+    return 0;
+}
+/*
+OUTPUT 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+*/
